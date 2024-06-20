af3e23a05b2dc9a75848d70013065c6f7c3e1229
dreamergirl01 <rahma01twins@gmail.com>
2024-06-20 11:22:57 +0700

    membuat modal menggunakan jquery

---
+++ modal_dialog_jquery/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#tombol').click(function(){
+        $('#modal-box').show(2000);
+        $('#background').fadeIn(1000);
+    })
+    $('#close-button').click(function(){
+        $('#modal-box').hide(2000);
+        $('#background').fadeOut(1000);
+    })
+})
