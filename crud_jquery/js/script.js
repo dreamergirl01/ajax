7cacdc12215817ea8d861d7f08a1b0a41f4a2b56
dreamergirl01 <rahma01twins@gmail.com>
2024-07-22 16:00:54 +0700

    tambahan proses edit dan delete

--- crud_jquery/js/script.js
+++ crud_jquery/js/script.js
@@ -5,8 +5,19 @@ $(document).ready(function(){
         tambahData();
     })
 
+    $('.btn_ubah').click(function(){
+        ubahData();
+    })
+
+    $('.btn_batal').click(function(){
+        bacaData();
+    })
+
     function bacaData(){
         $('.targetData').html('');
+        $('.btn_tambah').show();
+        $('.btn_ubah').hide();
+        $('.btn_batal').hide();
         $.ajax({
             type : 'GET',
             url : 'php/getData.php',
@@ -22,10 +33,22 @@ $(document).ready(function(){
                     <td>`+response[i].nama_barang+`</td>
                     <td>`+response[i].harga_barang+`</td>
                     <td>`+response[i].stok+`</td>
+                    <td>
+                    <button class='btn-edit' id='`+response[i].id+`'>Edit</button>
+                    <button class='btn-delete' id='`+response[i].id+`'>Delete</button>
+                    </td>
                     </tr>
                     `
                 }
                 $('.targetData').append(data)
+
+                $('.btn-edit').click(function(){
+                    getSingleData($(this).attr('id'));
+                })
+
+                $('.btn_delete').click(function(){
+                    deleteData($(this).attr('id'));
+                })
             }
         })
     }
@@ -53,7 +76,70 @@ $(document).ready(function(){
         })
     }
 
+    //untuk tampilkan data edit
+    function getSingleData(x){
+        $.ajax({
+            type : 'POST',
+            url : 'php/getSingleData.php',
+            data : 'id='+x,
+            dataType : 'JSON',
+            success : function(response){
+                $('.txt_id').val(response.id);
+                $('.txt_nama_barang').val(response.nama_barang);
+                $('.txt_harga_barang').val(response.harga_barang);
+                $('.txt_stok').val(response.stok);
+                $('.btn_tambah').hide();
+                $('.btn_ubah').show();
+                $('.btn_batal').show();
+            }
+        })
+    }
+
+    //untuk proses edit
+    function ubahData(){
+        var id = $('.txt_id').val();
+        var nama_barang = $('.txt_nama_barang').val();
+        var harga_barang = $('.txt_harga_barang').val();
+        var stok = $('.txt_stok').val();
+        $.ajax({
+            type : 'POST',
+            url : 'php/editData.php',
+            data : 'id='+id+'&nama_barang='+nama_barang+'&harga_barang='+harga_barang+'&stok='+stok,
+            dataType : 'JSON',
+            success : function(response){
+                if(response.status == '1'){
+                    alert(response.msg);
+                    bacaData();
+                    resetForm();
+                }else{
+                    alert(response.msg);
+                    bacaData();
+                    resetForm();
+                }
+
+            }
+        })
+    }
+
+    function deleteData(x){
+        $.ajax({
+            type : 'POST',
+            url : 'php/deleteData.php',
+            data : 'id='+x,
+            dataType : 'JSON',
+            success : function(response){
+                if(response.status == '1'){
+                    alert(response.msg);
+                    bacaData();
+                }else{
+                    alert(response.msg);
+                }
+            }
+        })
+    }
+
     function resetForm(){
+        $('.txt_id').val('');
         $('.txt_nama_barang').val('');
         $('.txt_harga_barang').val('');
         $('.txt_stok').val('');
